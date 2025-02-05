b5e2fa975873c76108ed2d00430f71d40c0e68ae
Y0U5S3F <akim25486@gmail.com>
2025-02-05 12:44:02 +0100

    tuto

--- Backend/jumparkrh/urls.py
+++ Backend/jumparkrh/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for jumparkrh project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from personnel.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('personnel/user/create/', CreateUserView.as_view(), name='create_user'),
+    path('personnel/token', TokenObtainPairView.as_view(), name='get_token'),
+    path('personnel/token/refresh', TokenRefreshView.as_view(), name='refresh_token'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
