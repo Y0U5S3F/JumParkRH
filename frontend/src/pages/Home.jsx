af7db9bf959d156a12e38d6ae6a5a2b69631fd46
YoNexuss <oussemazarrouki01@gmail.com>
2025-02-11 17:53:39 +0100

    Personal info add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
-import { Container, TextField, Button, Box, Modal, Typography, MenuItem, Divider, Grid } from "@mui/material";
+import { Container, TextField, Button, Box, Modal, Typography, MenuItem, Divider, Grid2, Select, FormControl,InputLabel } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+
 
 const useStyles = makeStyles(() => ({
   container: { padding: "20px", display: 'flex', flexDirection: 'column' },
@@ -12,7 +18,7 @@ const useStyles = makeStyles(() => ({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 700,
+    width: 1000,
     backgroundColor: 'black',
     boxShadow: 24,
     p: 4,
@@ -38,6 +44,7 @@ export default function Home() {
           service: emp.service ? emp.service.nom : "N/A"
         }));
         setEmployees(formattedEmployees);
+        console.log("Employees fetched:", formattedEmployees);
       } catch (error) {
         console.error("Error fetching employees:", error);
       }
@@ -79,18 +86,71 @@ export default function Home() {
         <Box className={classes.modalStyle}>
           <Typography variant="h6" gutterBottom>Veuillez saisir les coordonnées de vos personnels</Typography>
           <Divider sx={{ mb: 2 }} />
-          
-          <Grid container spacing={2}>
-            <Grid item xs={6}><TextField label="Nom" name="nom" fullWidth onChange={handleInputChange} /></Grid>
-            <Grid item xs={6}><TextField label="Prénom" name="prenom" fullWidth onChange={handleInputChange} /></Grid>
-            <Grid item xs={6}><TextField label="Email" name="email" fullWidth onChange={handleInputChange} /></Grid>
-            <Grid item xs={6}><TextField label="Matricule" name="matricule" fullWidth onChange={handleInputChange} /></Grid>
-            <Grid item xs={6}><TextField label="Rôle" name="role" select fullWidth onChange={handleInputChange}>
-              <MenuItem value="Admin">Admin</MenuItem>
-              <MenuItem value="Manager">Manager</MenuItem>
-              <MenuItem value="Employé">Employé</MenuItem>
-            </TextField></Grid>
-          </Grid>
+          <Typography variant="body1" color="white" gutterBottom>Personal Information:</Typography>
+          <Divider sx={{ mb: 2 }} />
+          <Grid2 container spacing={2}>
+            <Grid2 size={4} >
+              <TextField
+              id="outlined-search"
+              label="Matricule"
+              type="search"
+              variant="outlined"
+              fullWidth
+              />            
+            </Grid2>
+            <Grid2 size={4} >
+              <TextField
+              id="outlined-search"
+              label="Nom"
+              type="search"
+              variant="outlined"
+              expanded
+              fullWidth
+              />
+            </Grid2>
+            <Grid2 size={4} >
+              <TextField
+              id="outlined-search"
+              label="Prenom"
+              type="search"
+              variant="outlined"
+              expanded
+              fullWidth
+              />
+            </Grid2><Grid2 size={4} >
+              <TextField
+              id="outlined-search"
+              label="Email"
+              type="search"
+              variant="outlined"
+              expanded
+              fullWidth
+              
+              />
+      
+            </Grid2>
+            <Grid2 size={4} >
+            <FormControl fullWidth variant="outlined">
+          <InputLabel>Role</InputLabel>
+          <Select
+            label="Role"
+            defaultValue=""
+            name="role"
+          >
+            <MenuItem value="Admin">Admin</MenuItem>
+            <MenuItem value="Manager">Manager</MenuItem>
+            <MenuItem value="Employee">Employee</MenuItem>
+          </Select>
+        </FormControl>
+            </Grid2>
+            <Grid2 size={4} >
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker', 'DatePicker']}>
+        <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2022-04-17')} />
+      </DemoContainer>
+    </LocalizationProvider>
+          </Grid2>
+          </Grid2>
           
           <Box mt={3} display="flex" justifyContent="space-between">
             <Button variant="outlined" onClick={() => setNewEmployee({})}>Réinitialiser</Button>
