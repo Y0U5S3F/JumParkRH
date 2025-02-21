d3f266518e5691d30e7cbbd0bdd4dfd1eb0f289c
Y0U5S3F <akim25486@gmail.com>
2025-02-21 16:34:06 +0100

    calendar working with stream

--- Backend/app/conge/models.py
+++ Backend/app/conge/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from employe.models import Employe
+from typeconge.models import TypeConge
 
 class Conge(models.Model):
-    TYPE_CONGE_CHOICES = [
-        ('conge paye', 'Congé Payé'),
-        ('conge sans solde', 'Congé Sans Solde'),
-        ('conge maladie', 'Congé Maladie'),
-        ('conge maternite', 'Congé Maternité'),
-        ('conge paternite', 'Congé Paternité'),
-        ('conge exceptionnel', 'Congé Exceptionnel'),
-        ('conge annuel', 'Congé Annuel'),
-    ]
-
     STATUS_CHOICES = [
         ('en cours', 'En Cours'),
         ('accepte', 'Accepté'),
@@ -19,7 +10,7 @@ class Conge(models.Model):
     ]
 
     employe = models.ForeignKey(Employe, on_delete=models.PROTECT, related_name="conges", verbose_name="Employé")
-    type_conge = models.CharField(max_length=50, choices=TYPE_CONGE_CHOICES, verbose_name="Type de Congé")
+    type_conge = models.ForeignKey(TypeConge, on_delete=models.PROTECT, related_name="conges", verbose_name="Type de Congé")
     start_date = models.DateField(null=False, blank=False, verbose_name="Date de Début")
     end_date = models.DateField(null=False, blank=False, verbose_name="Date de Fin")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Statut")
