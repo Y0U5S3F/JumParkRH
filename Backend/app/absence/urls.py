35c7538a4293f5c832defe4a2994665bcf9c6588
Y0U5S3F <akim25486@gmail.com>
2025-03-12 15:34:50 +0100

    added dashboard api

--- Backend/app/absence/urls.py
+++ Backend/app/absence/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AbsenceListCreateView, AbsenceRetrieveUpdateDestroyView
+from .views import AbsenceListCreateView, AbsenceRetrieveUpdateDestroyView, dashboard_stats
 
 urlpatterns = [
     path('absences/', AbsenceListCreateView.as_view(), name='absence-list'),
     path('absences/<int:pk>/', AbsenceRetrieveUpdateDestroyView.as_view(), name='absence-retrieve-update-destroy-view'),
+    path('dashboard/', dashboard_stats, name='dashboard-stats'),
 ]
